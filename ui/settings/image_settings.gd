a16c837e435d8458c03976c6998abe36e1e7af90
namelessvoid <simon_kerler@web.de>
2025-06-17 00:27:03 +0200

    feat: Implement file dialog to chose image store location

--- ui/settings/image_settings.gd
+++ ui/settings/image_settings.gd
@@ -1,9 +1,27 @@
 extends Control
 
 @onready var _image_store_path_edit := %ImageStorePathEdit as LineEdit
+@onready var _image_store_path_select_button := %ImageStorePathSelectButton as Button
 
 func _ready() -> void:
 	_image_store_path_edit.text = Settings.get_setting(Settings.IMAGE_STORE, Settings.IMAGE_STORE_PATH)
-	_image_store_path_edit.text_changed.connect(func(p_value: String):
-		Settings.set_setting(Settings.IMAGE_STORE, Settings.IMAGE_STORE_PATH, p_value)
+	_image_store_path_edit.text_changed.connect(_save_image_store_path)
+	
+	_image_store_path_select_button.pressed.connect(_show_image_store_path_dialog)
+
+func _save_image_store_path(p_path: String):
+	Settings.set_setting(Settings.IMAGE_STORE, Settings.IMAGE_STORE_PATH, p_path)
+
+func _show_image_store_path_dialog():
+	var dialog := FileDialog.new()
+	add_child(dialog)
+	dialog.use_native_dialog = true
+	dialog.file_mode = FileDialog.FILE_MODE_OPEN_DIR
+	dialog.dir_selected.connect(func(p_dir: String):
+		_image_store_path_edit.text = p_dir
+		_save_image_store_path(p_dir)
+		dialog.queue_free()
 	)
+	dialog.canceled.connect(dialog.queue_free)
+	dialog.popup_centered_ratio(0.8)
+	
