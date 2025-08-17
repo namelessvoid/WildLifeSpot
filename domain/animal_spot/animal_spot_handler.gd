6fdf81ebfbca2d28e731f68bd289c01a31b7aab3
namelessvoid <simon_kerler@web.de>
2025-08-17 23:04:11 +0200

    refac: Add 'Query' suffix

--- domain/animal_spot/animal_spot_handler.gd
+++ domain/animal_spot/animal_spot_handler.gd
@@ -7,7 +7,7 @@ func can_handle(dispatchable: Variant) -> bool:
 		|| dispatchable is FindAllAnimalSpotDatesQuery \
 		|| dispatchable is FindAllAnimalSpotsByQuery \
 		|| dispatchable is FindAllAnimalSpotsByDateQuery \
-		|| dispatchable is FindAllAnimalSpotAnimalNames
+		|| dispatchable is FindAllAnimalSpotAnimalNamesQuery
 
 func handle(dispatchable: Variant) -> Variant:
 	if dispatchable is DeleteExistingAnimalSpots:
@@ -18,7 +18,7 @@ func handle(dispatchable: Variant) -> Variant:
 		return repository.find_all_by(dispatchable._source, dispatchable._camera_id, dispatchable._spotted_at)
 	elif dispatchable is FindAllAnimalSpotsByDateQuery:
 		return repository.find_all_by_date(dispatchable._date)
-	elif dispatchable is FindAllAnimalSpotAnimalNames:
+	elif dispatchable is FindAllAnimalSpotAnimalNamesQuery:
 		return repository.find_all_distinct_animal_names()
 
 	return null
