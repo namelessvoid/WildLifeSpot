3af654c3d2f7503c150d8bd808147ad5e2e4fd11
namelessvoid <simon_kerler@web.de>
2025-08-17 23:04:41 +0200

    feat: Fill animal settings + add color settings

--- domain/settings/settings.gd
+++ domain/settings/settings.gd
@@ -2,6 +2,8 @@ extends Node
 
 const IMAGE_STORE = &"image_store"
 const IMAGE_STORE_PATH = &"path"
+const ANIMALS = &"animals"
+
 const DEFAULTS = {
 	IMAGE_STORE: {
 		IMAGE_STORE_PATH: &"user://images"
@@ -28,3 +30,18 @@ func set_setting(p_section: StringName, p_setting: StringName, p_value: Variant)
 
 	if error != OK:
 		push_error("Failed to save config file. Error code: %s" % error)
+
+func get_or_create_animal_setting(p_animal_name: String) -> Dictionary:
+	var animal_setting: Dictionary = _config_file.get_value(ANIMALS, p_animal_name, {})
+	if animal_setting.is_empty():
+		animal_setting = { "color": _random_color(), "icon_path": "" }
+		set_setting(ANIMALS, p_animal_name, animal_setting)
+
+	return animal_setting
+
+func _random_color() -> Color:
+	return Color.from_hsv(
+		randf(),
+		randf_range(0.2, 0.6),
+		randf_range(0.9, 1.0),
+	)
--- ui/settings/animal_settings.gd
+++ ui/settings/animal_settings.gd
@@ -3,9 +3,29 @@ extends Control
 const _animal_pictogram_search_scene := preload("res://ui/animal_pictogram_search/animal_pictogram_search.tscn")
 
 @onready var _stack_container: StackContainer = $StackContainer
+@onready var _animal_tree: VBoxContainer = %AnimalSettingsContainer
 
 func _ready() -> void:
-	$StackContainer/VBoxContainer/PhylopicButton.pressed.connect(func():
-		var scene := _animal_pictogram_search_scene.instantiate()
-		_stack_container.push(scene)
-	)
+	var query := FindAllAnimalSpotAnimalNamesQuery.new()
+	var animal_names: PackedStringArray = CommandQueryDispatcher.dispatch(query)
+
+	for animal_name in animal_names:
+		var animal_setting := Settings.get_or_create_animal_setting(animal_name)
+		var control := _create_animal_settings_control(animal_name, animal_setting)
+		_animal_tree.add_child(control)
+
+func _create_animal_settings_control(p_animal_name: String, p_animal_setting: Dictionary) -> Control:
+	var hbox := HBoxContainer.new()
+
+	var color_picker_button := ColorPickerButton.new()
+	color_picker_button.color = p_animal_setting["color"]
+	color_picker_button.flat = true
+	color_picker_button.text = " "
+	color_picker_button.custom_minimum_size = Vector2(26, 0)
+	hbox.add_child(color_picker_button)
+
+	var name_label := Label.new()
+	name_label.text = p_animal_name
+	hbox.add_child(name_label)
+
+	return hbox
