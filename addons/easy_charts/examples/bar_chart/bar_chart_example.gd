4fce156ebfe13301471c468c835f75d52e931218
namelessvoid <simon_kerler@web.de>
2025-08-23 17:34:06 +0200

    chore: Update easy charts addon

--- addons/easy_charts/examples/bar_chart/bar_chart_example.gd
+++ addons/easy_charts/examples/bar_chart/bar_chart_example.gd
@@ -20,12 +20,7 @@ var cp: ChartProperties
 func _ready():
 	# Let's customize the chart properties, which specify how the chart
 	# should look, plus some additional elements like labels, the scale, etc...
-	cp = ChartProperties.new()
-	cp.colors.frame = Color("#161a1d")
-	cp.colors.background = Color.TRANSPARENT
-	cp.colors.grid = Color("#283442")
-	cp.colors.ticks = Color("#283442")
-	cp.colors.text = Color.WHITE_SMOKE
+	var cp = ChartProperties.new()
 	cp.y_scale = 10
 	cp.draw_origin = true
 	cp.draw_bounding_box = false
