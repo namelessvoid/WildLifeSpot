3658efc7bec85b502151d11be2e718c581a85d10
namelessvoid <simon_kerler@web.de>
2025-09-16 23:44:04 +0200

    feat: Add rudimentary pictogram selection

--- ui/animal_pictogram_search/animal_pictogram_search.gd
+++ ui/animal_pictogram_search/animal_pictogram_search.gd
@@ -1,11 +1,19 @@
 extends Control
 
+signal pictogram_selected(image: Image)
+
 @export var gbif_client: GBIFClient
 @export var phylopic_client: PhylopicClient
+
+var animal_name: String:
+	get: return _search_edit.text
+	set(p_value): _search_edit.text = p_value
 
 @onready var _search_edit: LineEdit = %SearchEdit
 @onready var _search_button: Button = %SearchButton
 @onready var _result_tree: Tree = %ResultTree
+
+var _images: Array[Image] = []
 
 func _ready() -> void:
 	assert(_search_edit)
@@ -16,6 +24,7 @@ func _ready() -> void:
 	assert(phylopic_client)
 
 	_search_button.pressed.connect(_on_search_button_pressed)
+	_result_tree.button_clicked.connect(_on_result_tree_button_clicked)
 
 func _on_search_button_pressed() -> void:
 	_search_button.text = "Searching..."
@@ -30,15 +39,22 @@ func _on_search_button_pressed() -> void:
 	var root = _result_tree.create_item()
 
 	for item in result.items:
-		var tree_item := root.create_child()
-		tree_item.set_text(1, item.canonical_name)
-
 		var image := await phylopic_client.find_picture_for_gbif_item(item)
+		_images.append(image)
+
 		var texture := ImageTexture.create_from_image(image)
+		
+		var tree_item := root.create_child()
 		tree_item.set_icon(0, texture)
 		tree_item.set_icon_max_width(0, 48)
+		tree_item.set_text(1, item.canonical_name)
+		tree_item.add_button(2, load("res://icons/question_mark.png"))
 
 	_search_button.text = "Search"
 	_search_button.disabled = false
 	_search_edit.editable = true
 	_result_tree.visible = true
+
+func _on_result_tree_button_clicked(item: TreeItem, _column: int, _id: int, _mouse_button_index: int):
+	var image: Image = _images[item.get_index()]
+	pictogram_selected.emit(image)
--- ui/settings/animal_settings.gd
+++ ui/settings/animal_settings.gd
@@ -1,5 +1,6 @@
 extends Control
 
+const AnimalPictogramSearch := preload("res://ui/animal_pictogram_search/animal_pictogram_search.gd")
 const _animal_pictogram_search_scene := preload("res://ui/animal_pictogram_search/animal_pictogram_search.tscn")
 
 @onready var _stack_container: StackContainer = $StackContainer
@@ -38,7 +39,7 @@ func _create_animal_settings_control(p_animal_name: String, p_animal_setting: Di
 	icon_button.icon = load("res://icons/question_mark.png")
 	icon_button.flat = true
 	icon_button.pressed.connect(func():
-		_on_icon_button_pressed(p_animal_name, p_animal_setting)
+		_on_icon_button_pressed(p_animal_name,)
 	)
 	hbox.add_child(icon_button)
 
@@ -49,6 +50,18 @@ func _on_animal_color_picker_color_changed(p_animal_name: String, p_color: Color
 	animal_setting["color"] = p_color
 	Settings.set_setting(Settings.ANIMALS, p_animal_name, animal_setting)
 
-func _on_icon_button_pressed(p_animal_name: String, p_animal_setting: Dictionary) -> void:
-	var scene = _animal_pictogram_search_scene.instantiate()
+func _on_icon_button_pressed(p_animal_name: String) -> void:
+	var scene: AnimalPictogramSearch = _animal_pictogram_search_scene.instantiate()
 	_stack_container.push("Pictogram search", scene)
+	scene.animal_name = p_animal_name
+	scene.pictogram_selected.connect(func(p_image: Image):
+		_on_pictogram_selected(p_animal_name, p_image)
+	)
+
+func _on_pictogram_selected(p_animal_name: String, p_image: Image) -> void:
+	DirAccess.make_dir_absolute("user://animal_pictograms")
+	var animal_setting := Settings.get_or_create_animal_setting(p_animal_name)
+	var path := "user://animal_pictograms/%s.png" % p_animal_name
+	p_image.save_png(path)
+	animal_setting["icon_path"] = path
+	Settings.set_setting(Settings.ANIMALS, p_animal_name, animal_setting)
