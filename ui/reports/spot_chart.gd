544852d9a92fdef23cdd4bc197cd394abfaff540
namelessvoid <simon_kerler@web.de>
2025-09-16 23:56:19 +0200

    feat: Animal pictograms are colord in chart

--- ui/reports/spot_chart.gd
+++ ui/reports/spot_chart.gd
@@ -69,6 +69,12 @@ func _get_spot_plot_function(animal: String, animal_setting: Dictionary, spots_p
 	var icon: Texture2D = null
 	if animal_setting["icon_path"]:
 		var image := Image.load_from_file(animal_setting["icon_path"])
+		for x in image.get_width():
+			for y in image.get_height():
+				var previous_color = image.get_pixel(x, y)
+				var new_color = Color.WHITE - previous_color
+				new_color.a = previous_color.a
+				image.set_pixel(x, y, new_color)
 		icon = ImageTexture.create_from_image(image)
 
 	return Function.new(
