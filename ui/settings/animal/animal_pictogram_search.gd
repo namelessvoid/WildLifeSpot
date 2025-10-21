a9a2aa4dfb5dfbab763a082a5e07838521a08da7
namelessvoid <simon_kerler@web.de>
2025-10-21 23:13:18 +0200

    ui: Refac pictogram selection into own component

--- ui/settings/animal/animal_pictogram_search.gd
+++ ui/settings/animal/animal_pictogram_search.gd
@@ -11,50 +11,53 @@ var animal_name: String:
 
 @onready var _search_edit: LineEdit = %SearchEdit
 @onready var _search_button: Button = %SearchButton
-@onready var _result_tree: Tree = %ResultTree
+@onready var _result_container: Container = %ResultContainer
+
+const AnimalPictogramSelectLine := preload("uid://ccoq3tj2phxr8")
+const _animal_pictogram_selection_line_scene := preload("uid://deswauwup3dfq")
 
 var _images: Array[Image] = []
 
 func _ready() -> void:
 	assert(_search_edit)
 	assert(_search_button)
-	assert(_result_tree)
+	assert(_result_container)
 
 	assert(gbif_client)
 	assert(phylopic_client)
 
 	_search_button.pressed.connect(_on_search_button_pressed)
-	_result_tree.button_clicked.connect(_on_result_tree_button_clicked)
+	#_result_tree.button_clicked.connect(_on_result_tree_button_clicked)
 
 func _on_search_button_pressed() -> void:
 	_search_button.text = "Searching..."
 	_search_button.disabled = true
 	_search_edit.editable = false
-	_result_tree.visible = false
+	_result_container.visible = false
 
 	var vernacular_name := _search_edit.text
 	var result: GBIFSearchResult = await gbif_client.search_by_vernacular_name(vernacular_name)
 
-	_result_tree.clear()
-	var root = _result_tree.create_item()
+	for child in _result_container.get_children():
+		_result_container.remove_child(child)
+		child.queue_free()
 
 	for item in result.items:
 		var image := await phylopic_client.find_picture_for_gbif_item(item)
 		_images.append(image)
 
-		var texture := ImageTexture.create_from_image(image)
-		
-		var tree_item := root.create_child()
-		tree_item.set_icon(0, texture)
-		tree_item.set_icon_max_width(0, 48)
-		tree_item.set_text(1, item.canonical_name)
-		tree_item.add_button(2, load("res://icons/question_mark.png"))
+		var select_line: AnimalPictogramSelectLine = _animal_pictogram_selection_line_scene.instantiate()
+		_result_container.add_child(select_line)
+		select_line.pictogram_texture = ImageTexture.create_from_image(image)
+		select_line.animal_name = item.canonical_name
+		select_line.pictogram_selected.connect(func():
+			_on_pictogram_selected(image)
+		)
 
 	_search_button.text = "Search"
 	_search_button.disabled = false
 	_search_edit.editable = true
-	_result_tree.visible = true
+	_result_container.visible = true
 
-func _on_result_tree_button_clicked(item: TreeItem, _column: int, _id: int, _mouse_button_index: int):
-	var image: Image = _images[item.get_index()]
+func _on_pictogram_selected(image: Image):
 	pictogram_selected.emit(image)
---
+++ ui/settings/animal/animal_pictogram_select_line.gd
@@ -0,0 +1,18 @@
+extends HBoxContainer
+
+signal pictogram_selected
+
+@onready var _animal_icon_texture_rect: TextureRect = $AnimalIconTextureRect
+@onready var _animal_name_label: Label = $AnimalNameLabel
+@onready var _choose_icon_button: Button = $ChooseIconButton
+
+var pictogram_texture: Texture2D:
+	get: return _animal_icon_texture_rect.texture
+	set(value): _animal_icon_texture_rect.texture = value
+
+var animal_name: String:
+	get: return _animal_name_label.text
+	set(value): _animal_name_label.text = value
+
+func _ready():
+	_choose_icon_button.pressed.connect(pictogram_selected.emit)
