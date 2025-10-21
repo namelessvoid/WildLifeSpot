f74249eb2cb439fa8b6f70aafaeb45549c942cbe
namelessvoid <simon_kerler@web.de>
2025-10-21 21:01:49 +0200

    chore: Formatting

--- infrastructure/ExifInfo.cs
+++ infrastructure/ExifInfo.cs
@@ -1,10 +1,9 @@
 using Godot;
-using System;
 
 [GlobalClass]
 public partial class ExifInfo : RefCounted
 {
-	public string DateTime { get; set; }
-	
-	public string CameraMake { get; set; }
+    public string DateTime { get; set; }
+
+    public string CameraMake { get; set; }
 }
