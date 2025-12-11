25b0c9ea8a7c90fbb532d37588487a9b054f8536
namelessvoid <simon_kerler@web.de>
2025-12-12 00:11:34 +0100

    test: Assert signal call order

--- test/unit/ui/components/test_validatable_line_edit.gd
+++ test/unit/ui/components/test_validatable_line_edit.gd
@@ -33,3 +33,21 @@ func test_should_validate_and_update_validation_label():
 	# Assert
 	assert_true(validatable_line_edit.is_valid())
 	assert_eq(validatable_line_edit._validation_message_label.text, "")
+
+func test_should_emit_signals_in_correct_order():
+	# Arrange
+	var validatable_line_edit: ValidatableLineEdit = _validatable_line_edit.instantiate()
+	add_child_autofree(validatable_line_edit)
+	validatable_line_edit.validator = func(_p): return false
+	watch_signals(validatable_line_edit)
+
+	var signal_calls: Array[String] = []
+	validatable_line_edit.validity_changed.connect(func(_p): signal_calls.append("validity_changed"))
+	validatable_line_edit.text_changed.connect(func(_p): signal_calls.append("text_changed"))
+
+	# Act
+	validatable_line_edit.text = "invalid input"
+	validatable_line_edit._line_edit.editing_toggled.emit(false)
+
+	# Assert
+	assert_eq(signal_calls, ["validity_changed", "text_changed"])
